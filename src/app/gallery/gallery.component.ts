0a53d73e9db0d98b5a60f03c0e53491bf4e0f4a1
jrcaranddang <jrcaranddang@gmail.com>
2017-05-10 18:48:11 -0700

    added full image view to gallery, working on responsive media queries

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,3 +1,4 @@
+import { Output } from '@angular/core/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,25 +7,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
+  clicked: Boolean = false;
+  fullImage = '';
   tiles = [
-    {text: 'One', cols: 3, rows: 5, color: '#FFF5EE', img: "url('../../assets/img/IMG_0352.JPG')"},
-    {text: 'Two', cols: 3, rows: 3, color: '#FFF5E1', img: "url('../../assets/img/IMG_0364.JPG')"},
-    {text: 'Three', cols: 3, rows: 5, color: '#E8DCCD', img: "url('../../assets/img/IMG_0384.JPG')"},
-    {text: 'Four', cols: 3, rows: 5, color: '#E8D4CD', img: "url('../../assets/img/whiteCabinetEdit.jpg')"},
-    {text: 'Five', cols: 3, rows: 6, color: '#FFE5E1', img: "url('../../assets/img/IMG_0390.JPG')"},
-    {text: 'Six', cols: 3, rows: 6, color: '#FFF5EE', img: "url('../../assets/img/IMG_0427.JPG')"},
-    {text: 'Seven', cols: 3, rows: 6, color: '#FFF5E1', img: "url('../../assets/img/20160923_170322.jpg')"},
-    {text: 'Eight', cols: 3, rows: 6, color: '#E8DCCD', img: "url('../../assets/img/20160824_152620.jpg')"},
-    {text: 'Nine', cols: 3, rows: 6, color: '#E8D4CD', img: "url('../../assets/img/20170317_141722.jpg')"},
-    {text: 'Ten', cols: 3, rows: 3, color: '#FFE5E1', img: "url('../../assets/img/counterTopCloseUpEdit.jpg')"},
-    {text: 'Eleven', cols: 3, rows: 6, color: '#FFF5EE', img: "url('../../assets/img/fullKitchenRemoveBottleLabels.jpg')"},
-    {text: 'Tweleve', cols: 3, rows: 5, color: '#FFF5E1', img: "url('../../assets/img/IMG_0420.JPG')"},
-    {text: 'Five', cols: 3, rows: 5, color: '#E8DCCD', img: "url('../../assets/img/IMG_0412.JPG')"},
-    {text: 'Thirteen', cols: 3, rows: 5, color: '#E8D4CD', img: "url('../../assets/img/rightSideKitchenRemoveBottleLabels.jpg')"},
+    {num: 'One', cols: 3, rows: 5, color: '#FFF5EE', img: "url('../../assets/img/IMG_0352.JPG')"},
+    {num: 'Two', cols: 3, rows: 3, color: '#FFF5E1', img: "url('../../assets/img/IMG_0364.JPG')"},
+    {num: 'Three', cols: 3, rows: 5, color: '#E8DCCD', img: "url('../../assets/img/IMG_0384.JPG')"},
+    {num: 'Four', cols: 3, rows: 5, color: '#E8D4CD', img: "url('../../assets/img/whiteCabinetEdit.jpg')"},
+    {num: 'Five', cols: 3, rows: 6, color: '#FFE5E1', img: "url('../../assets/img/IMG_0390.JPG')"},
+    {num: 'Six', cols: 3, rows: 6, color: '#FFF5EE', img: "url('../../assets/img/IMG_0427.JPG')"},
+    {num: 'Seven', cols: 3, rows: 6, color: '#FFF5E1', img: "url('../../assets/img/20160923_170322.jpg')"},
+    {num: 'Eight', cols: 3, rows: 6, color: '#E8DCCD', img: "url('../../assets/img/20160824_152620.jpg')"},
+    {num: 'Nine', cols: 3, rows: 6, color: '#E8D4CD', img: "url('../../assets/img/20170317_141722.jpg')"},
+    {num: 'Ten', cols: 3, rows: 3, color: '#FFE5E1', img: "url('../../assets/img/counterTopCloseUpEdit.jpg')"},
+    {num: 'Eleven', cols: 3, rows: 6, color: '#FFF5EE', img: "url('../../assets/img/fullKitchenRemoveBottleLabels.jpg')"},
+    {num: 'Tweleve', cols: 3, rows: 5, color: '#FFF5E1', img: "url('../../assets/img/IMG_0420.JPG')"},
+    {num: 'Thirteen', cols: 3, rows: 5, color: '#E8DCCD', img: "url('../../assets/img/IMG_0412.JPG')"},
+    {num: 'Fourteen', cols: 3, rows: 5, color: '#E8D4CD', img: "url('../../assets/img/rightSideKitchenRemoveBottleLabels.jpg')"},
+  ];
+
+  images = [
+    {num: 'One', src: '../../assets/img/IMG_0352.JPG' },
+    {num: 'Two', src: '../../assets/img/IMG_0364.JPG' },
+    {num: 'Three', src: '../../assets/img/IMG_0384.JPG' },
+    {num: 'Four', src: '../../assets/img/whiteCabinetEdit.jpg' },
+    {num: 'Five', src: '../../assets/img/IMG_0390.JPG' },
+    {num: 'Six', src: '../../assets/img/IMG_0427.JPG' },
+    {num: 'Seven', src: '../../assets/img/20160923_170322.jpg' },
+    {num: 'Eight', src: '../../assets/img/20160824_152620.jpg' },
+    {num: 'Nine', src: '../../assets/img/20170317_141722.jpg' },
+    {num: 'Ten', src: '../../assets/img/counterTopCloseUpEdit.jpg' },
+    {num: 'Eleven', src: '../../assets/img/fullKitchenRemoveBottleLabels.jpg' },
+    {num: 'Tweleve', src: '../../assets/img/IMG_0420.JPG' },
+    {num: 'Thirteen', src: '../../assets/img/IMG_0412.JPG' },
+    {num: 'Fourteen', src: '../../assets/img/rightSideKitchenRemoveBottleLabels.jpg' }
   ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  viewImage(img) {
+    // console.log(img.slice(4, img.length-1))
+    this.clicked = true;
+    // this.fullImage = img.slice(4, img.length-1);
+
+    console.log(img.num);
+    this.images.forEach(image => {
+        // console.log(image.src);
+      if (img.num === image.num) {
+        console.log(image.src);
+        this.fullImage = image.src;
+      }
+    });
+  }
+
+  closeImage() {
+    this.clicked = false;
+  }
 }
